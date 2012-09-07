4a04d7ae6afc1ab5b032d06cbf5049a87bbb886c
Caio Wilson Bernabé <caiowilson@gmail.com>
2012-09-07 02:25:09 -0300

    mais alteraçoes e erros.

--- next-ocs-registration.php
+++ next-ocs-registration.php
@@ -8,37 +8,53 @@ Version: 1.12
 Author: Caio Wilson
 */
 
+
 add_filter('login_redirect', 'next_ocs_login');
 
 function next_ocs_login(){
-	if($_GET['action'] = 'register' && ($_GET['from_ocs'] )){
-		setcookie('from_ocs','true', time()+3600 );
-	}
+	
 	if($_GET['from_ocs']){
 
 		return home_url('/conferencias/');
 			
 	}
 	else if($_COOKIE['from_ocs'] == true){
-	
+
 		return home_url('/conferencias/');
 
 	}
-	
+
 
 }
 
-/* add_filter('wp_signup_location', 'next_ocs_register');
+add_filter('wp_signup_location', 'next_ocs_register');
 
 function next_ocs_register(){
-	//if($_COOKIE['from_ocs'] ($_GET['from_ocs'] ))
-	return site_url('wp-signup.php?from_ocs=true');
+//if($_COOKIE['from_ocs'] ($_GET['from_ocs'] ))
+	
+	setcookie('from_ocs','true', time()+3600 );
+	
+	/* if($_GET['action'] = 'register' && $_GET['from_ocs'] == 'true'){
+		setcookie('from_ocs','true', time()+3600 );
+	} */
+	return site_url('wp-signup.php');
 }
 
-add_action('before_signup_form', 'ocs_before_signup');
+/* add_action('login_init', 'ocs_login_init');
 
-function ocs_before_signup(){
-	echo 'CHEGUEI';
+function ocs_login_init(){
+	//echo 'TESTE'; 
+	if($_REQUEST['action'] == 'register' && $_REQUEST['from_ocs'] == 'true'){
+		
+		setcookie('from_ocs','true', 0 );
+	}
 } */
+
+add_action('after_signup_form', 'ocs_signup');
+
+function ocs_signup(){
+// 	setcookie('from_ocs','true', time()+3600 );
+	echo 'MALDITO HOOK QUE NAO VAI';
+}
 
 ?>
